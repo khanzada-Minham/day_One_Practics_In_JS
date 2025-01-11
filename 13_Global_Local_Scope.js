8aa96e850cf43e66c69dbcad24744efa72401196
Minham <minhamhussain@gmail.com>
2025-01-11 17:17:20 +0500

    11/01/2025

--- 13_Global_Local_Scope.js
+++ 13_Global_Local_Scope.js
@@ -14,3 +14,39 @@ console.log(a);
 // console.log(b);
 // console.log(c);
 
+function One() {
+    const username = "Minham";
+
+    function Two() {
+        let minhamPhone = 'Iphone'
+        console.log(username);
+    }
+    // console.log(minhamPhone);  //   Throw the Error not acces in scope
+    // Two()
+}
+
+// One()
+
+
+if (true) {
+    let username = "MINHAM";
+    if (username === "MINHAM") {
+        let age = 20;
+        console.log(username);
+    }
+    //console.log(age); // Throw the Error not acces in scope
+}
+// console.log(username);
+
+
+
+function addTwoNumber(num) {
+    return num + 1
+}
+console.log(addTwoNumber(10)); // 11
+
+
+const addNumber = function (num) {
+    return num + 10
+}
+console.log(addNumber(10)); // 20
