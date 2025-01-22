0d6b5b27815b1d5112f438217221523ed9b24c3d
Minham <minhamhussain@gmail.com>
2025-01-22 18:26:52 +0500

    22/01/2025

---
+++ 16_ControlFlow.js
@@ -0,0 +1,16 @@
+// if Statemint
+
+// LessThen  <, 
+// GraterThen >,
+// lessThan Equal  <=,
+// GreaterThen Equal  >=,
+// Equal  ==,
+// Not Equal  !=,
+// Not  !,
+// And  &&,
+// Or  ||,
+
+if (true){
+    console.log(`Hello Minham`);
+}
+console.log(`Hello Boy`);
