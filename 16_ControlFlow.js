16420e20dee62eff5fe4120afe752a5f2b9411a1
Minham <minhamhussain@gmail.com>
2025-01-24 17:05:21 +0500

    24/01/2025

--- 16_ControlFlow.js
+++ 16_ControlFlow.js
@@ -10,7 +10,53 @@
 // And  &&,
 // Or  ||,
 
-if (true){
-    console.log(`Hello Minham`);
+const temperature = 40;
+
+if (temperature < 40) {
+    console.log(`Temperature is lessthan 50 `);
 }
-console.log(`Hello Boy`);
+else {
+    console.log(`Temperature Grather then  50 `);
+}
+
+const score = 400;
+if (score > 100) {
+    let power = "flay"
+    console.log(`User Power: ${power}`);
+}
+
+const balance = 1000;
+// if(balance > 500) console.log(`User has enough balance to buy a product` );
+
+if (balance < 500) {
+    console.log(`less than 500`);
+}
+else if (balance < 750) {
+    console.log(`less than 750`);
+}
+else if (balance < 900) {
+    console.log(`less than 900`);
+}
+else {
+    console.log(`Balance is greater than 950`);
+}
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInGoogle = false;
+const loggedInEmail = true;
+
+if(userLoggedIn && debitCard){
+    console.log(`Allow to bay course`);
+}
+if (loggedInGoogle || loggedInEmail) {
+    // if(loggedInGoogle == true ){
+    //     console.log(`User LoggedIn Google`);
+    //     console.log(`User is LoggedIn`);
+    // }
+    // else{
+    //     console.log(`User LoggedIn Email`);
+    //     console.log(`User is LoggedIn`);
+    // }
+    console.log(`User is LoggedIn`);
+}
---
+++ 18_truthy.js
@@ -0,0 +1,39 @@
+// Falsy Value;
+
+// false ,0 , -0 , BigInt : 0n , "" , null , undefined , NaN;
+
+
+// Truthy Value:
+
+// "0", "false" , " " , [] , {} , function(){};
+
+const userEmail = "M@khanzada.ai";
+if(userEmail != ''){
+    console.log(`Got User Email`);
+}
+else{
+    console.log(`Don't have user Email `);
+}
+
+const emptyArray = [];
+if(emptyArray.length === 0){
+    console.log(`Array is empty`);
+}
+
+const emptyObj = {}
+if(Object.keys(emptyObj).length === 0){
+    console.log(`Object is empty` );
+}
+// Nullish Coalescing Operator (??) :null undefined;
+
+let vel1;
+// vel1 = 5 ?? 10;
+// vel1 = null ?? 10;
+vel1 = undefined ?? 10;
+console.log(vel1); 
+
+// Terniary Operator (?);
+// Condition ? true : false;
+
+const icePrices = 100;
+icePrices <= 80 ? console.log(`less then 80`) : console.log(`more then 80` );
