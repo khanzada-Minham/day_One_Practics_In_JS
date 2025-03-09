6c919c8376ecf573083299e1706c5a288c820232
Minham <minhamhussain@gmail.com>
2025-03-09 07:39:29 +0500

    09/03/2025

---
+++ Prictice_In_JavaScript/24_StringMethod.js
@@ -0,0 +1,11 @@
+let userName = 'Minham'
+console.log(userName.length);
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
+// let result = userName.slice(2, 4);
+// let result = userName.split('').reverse().join('').toUpperCase();
+// let result = userName.replace('Minham','Khanzada');
+let result = userName;
+console.log(result);
+
+
