2dff5614929b8e464bfa3ea6a5e1900a0b9e7dac
Minham <minhamhussain@gmail.com>
2025-03-09 09:26:40 +0500

    09/03/2025

---
+++ DSA_JavaScript/DSA_Question.js
@@ -0,0 +1,81 @@
+// Q1
+
+// let a = 10;
+// let b = `20`;
+// let result =  a + b;
+// console.log(result);
+
+// Q2
+
+// let a = 10;
+// let b = 20;
+// console.log('some of 10 and 20 is ' + (a + b));
+
+
+// Q3
+
+// let a = 1;
+// let b = '1';
+// console.log(a - b);
+
+
+// Q4 Part One
+
+// let a = 10;
+// let b = 20;
+// let c = a; 
+
+// b = b - c
+// a = a + b
+
+// console.log(a);
+// console.log(b);
+
+// Q4 Part Two
+
+// let a = 10;
+// let b = 20;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log(a);
+// console.log(b);
+
+// Q4 Part Three
+
+// let a = 10;
+// let b = 20;
+
+// let c = a; // C 10
+// a = b // a 20
+// b = c // b 10
+
+
+// console.log(a);
+// console.log(b);
+
+// Q4 Part Fore
+
+// let a = 10;
+// let b = 20;
+
+// [a, b] = [b, a]
+
+// console.log(a);
+// console.log(b);
+
+// Q5
+
+// let a = 17;
+// let b = 5;
+
+// console.log(Math.floor(a / b));
+// console.log(a % b);
+
+// Q6
+
+// let a = 436;
+
+// console.log(a % 10);
