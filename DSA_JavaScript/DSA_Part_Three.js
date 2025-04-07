5cbf9c6d5475c58e2e568dbcb4692a953370c988
Minham <minhamhussain@gmail.com>
2025-04-07 14:35:05 +0500

    07/04/2025

---
+++ DSA_JavaScript/DSA_Part_Three.js
@@ -0,0 +1,42 @@
+// for(i = 0 ;i < 6001; i++){
+//     console.log(`${i} Hello`);   
+// }
+
+// 1 - 22;
+// for(i = 1 ; i < 23; i++){
+//     console.log(i);
+// }
+
+// 23 - 54
+
+// for(i = 23; i < 55; i++){
+//     console.log(i);
+// }
+
+// 200 - 100;
+// for(i = 20; i >= 10; i--){
+//     console.log(i);
+// }
+
+// var i = 1
+// for(; i <= 10; i++){
+//     console.log(i);
+// }
+// console.log(`Fail ${i}`);
+
+
+// let n = 20;
+// let sum = 0
+// for(let i = 0; i <= n; i++){
+//     sum += i 
+// }
+// console.log(sum);
+
+
+// let n = 1000;
+
+// for (let i = 0; i <= Math.floor(n / 2); i++) {
+//     if (n % i === 0) {
+//         console.log(i);
+//     }
+// }
