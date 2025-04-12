1c25239ba902fffc177848d45a0cb45b58402e31
Minham <minhamhussain@gmail.com>
2025-04-12 18:55:30 +0500

    12/04/2025

---
+++ DSA_JavaScript/DSA_Part_five.js
@@ -0,0 +1,2 @@
+// let fullName = "Minham";
+// console.log(fullName)
--- DSA_JavaScript/DSA_Part_fore.js
+++ DSA_JavaScript/DSA_Part_fore.js
@@ -69,3 +69,147 @@
 // }
 // console.log("Maximum number in the array is : " + max);
 // console.log("Second Maximum number in the array is : " + smax);
+
+// let arr = [39, 39, 39, 39, 39, 19];
+// let max = -Infinity;
+// let smax = -Infinity;
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     if (element > max) {
+//         smax = max;
+//         max = element;
+//     }
+//     else if (element > smax && max != element){
+//         smax = element
+//     }
+// }
+
+// console.log("Maximum number in the array is : " + max);
+// console.log("Second Maximum number in the array is : " + smax);
+
+
+// let arr = [10, 20, 30, 40, 50, 60];
+// let temp = new Array(arr.length);
+// let counter = 0
+// for (let i = arr.length -1 ; i >= 0 ; i--) {
+//     temp[counter] = arr[i];
+//     counter++;
+// }
+// console.log(temp);
+
+// let arr = [1, 2, 3, 4];
+// let newArr = new Array(arr.length);
+// let counter = 0;
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     newArr[counter] = arr[i]
+//     counter++
+// }
+// console.log(newArr);
+
+
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let newArr = new Array(arr.length);
+// let counter = 0;
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     newArr[counter] = arr[i];
+//     counter++
+// }
+// console.log(newArr);
+
+
+// let i = 0;
+// let j = arr.length - 1;
+// while (i < j) {
+//     let tamp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = tamp;
+//     i++;
+//     j--;
+// }
+// console.log(arr);
+
+
+
+// let arr = [0, 1, 0, 1, 0, 1, 0, 1, 0,];
+// let j = 0;
+// let i = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 0) {
+//         let tamp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = tamp;
+//         j++
+//     }
+// }
+// console.log(arr);
+
+// while (i < arr.length) {
+//     if (arr[i] === 0) {
+//         let tamp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = tamp;
+//         j++
+//     }
+//     i++
+// }
+// console.log(arr);
+
+
+// let arr = [10, 20, 30, 40, 50, 60, 5, 80, 90];
+// let min =  arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         min = arr[i]
+//     }
+// }
+// console.log(`Minimam Value of : ${min}`);
+
+// let arr = [10, 20, 30, 40, 50, 60, 5, 80, 90];
+// let max = Math.max(arr[0],arr[1]);
+// let smax = Math.min(arr[0],arr[1])
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < max) {
+//         smax = max;
+//         max = arr[i];
+//     }
+//     else if(smax > arr[i] && max > smax){
+//         smax = arr[i]
+//     }
+// }
+// console.log("Maximum number in the array is : " + max);
+// console.log("Second Maximum number in the array is : " + smax);
+
+
+// let arr = [-10, 8, -5, 17, -3, -8, 14, 5, 7, -2, -25];
+// let j = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//         let tamp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = tamp;
+//         j++
+//     }
+// }
+// console.log(arr);
+
+
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+// let newArr = new Array(arr.length);
+// let counter = 0;
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     newArr[counter] = arr[i];
+//     counter++
+// }
+// console.log(newArr);
+// let i = 0;
+// let j = arr.length-1;
+// while (i < j) {
+//     let tamp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = tamp
+//     i++
+//     j--
+// }
+// console.log(arr);
