469bf5ca7a1cd6aae0ba2bec473d00c560244272
Minham <minhamhussain@gmail.com>
2025-04-20 19:57:38 +0500

    20/04/2025

--- DSA_JavaScript/DSA_Part_Six_.js
+++ DSA_JavaScript/DSA_Part_Six_.js
@@ -9,10 +9,35 @@ for (let i = 0; i < arr.length - 1; i++) {
 }
 arr[arr.length - 1] = copy;
 console.log(arr);
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let copy = arr[0];
+for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+}
+arr[arr.length - 1] = copy;
+console.log(arr);
+
 */
 
 // Right Rotation By 1 Element;
 
 /*
-*/
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let copy = arr[arr.length - 1];
+for (let i = arr.length - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+}
+arr[0] = copy;
+console.log(arr);
 
+
+let arr = [2, 3, 4, 5, 6, 7, 1];
+let copy = arr[arr.length - 1];
+for (let i = arr.length - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+}
+arr[0] = copy;
+console.log(arr);
+
+*/
